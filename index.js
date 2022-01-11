7e024d35d6a45cadaf1649f78a04df5f74c010f5
Jinxuan Wu <jinxuanw@icloud.com>
2022-01-10 21:38:49 -0500

    done with todos

--- index.js
+++ index.js
@@ -1,43 +1,20 @@
-{
-  type: 'ADD_TODO',
-  todo: {
-    id: 0,
-    name: 'Learn Redux',
-    complete: false,
-  }
-}
-
-{
-  type: 'REMOVE_TODO',
-  id: 0,
-}
-
-{
-  type: 'TOGGLE_TODO',
-  id: 0,
-}
-
-{
-  type: 'ADD_GOAL',
-  goal: {
-    id: 0,
-    name: 'Run a Marathon'
-  }
-}
-
-{
-  type: 'REMOVE_GOAL',
-  id: 0
-}
-
+// Reducer function
 function todos(state = [], action) {
-  if (action.type === 'ADD_TOTO') {
-    return state.concat([action.todo]); // not push. 
+  switch(action.type) {
+    case 'ADD_TODO' :
+      return state.concat([action.todo])
+    case 'REMOVE_TODO' :
+      return state.filter((todo) => todo.id !== action.id)
+    case 'TOGGLE_TODO' :
+      return state.map((todo) => todo.id !== action.id ? todo :
+        Object.assign({}, todo, {complete: !todo.complete})
+      )
+    default :
+      return state
   }
-  return state;
 }
 
-function createStore () {
+function createStore (reducer) {
   // The store should have four parts
   // 1. The state
   // 2. Get the state.
@@ -56,8 +33,34 @@ function createStore () {
     }
   }
 
+  const dispatch = (action) => {    
+    state = reducer(state, action)    
+    listeners.forEach(listener => listener(state))
+  }
+  
   return {
     getState,
     subscribe,
+    dispatch
   }
 }
+
+const store = createStore(todos)
+store.subscribe((state) => {console.log(state)})
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 0,
+    name: 'Learn Redux',
+    complete: false,
+  }
+})
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 1,
+    name: 'Learn Redux1',
+    complete: true,
+  }
+})
